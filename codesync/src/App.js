ab06197b1c4c4de019b0d21070b1610fc7ebf9ff
Vishal Kumar Nigam <115461865+VishalKNigam@users.noreply.github.com>
2023-06-18 13:18:50 +0530

    Home page completed with all features. Now doing the EditorPage

--- codesync/src/App.js
+++ codesync/src/App.js
@@ -2,10 +2,29 @@ import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home';
 import EditorPage from './pages/EditorPage';
+import {Toaster} from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <div>
+      <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: '#4aed88',
+              color: '#fff',
+              padding: '16px',
+              borderRadius: '8px',
+              boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+              textAlign: 'center',
+            },
+            success: {
+              duration: 3000, // Set the duration for success toast messages
+            },
+          }}
+        />
+      </div>
        <BrowserRouter>
           <Routes>
             <Route path='/' element={<Home/>}></Route>
--- codesync/src/pages/Home.js
+++ codesync/src/pages/Home.js
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [roomID, setRoomID] = useState("");
+  const [username, setUsername] = useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomID(id);
+    toast.success("New Room created successful!!");
+  };
+  const joinRoom = ()=>{
+    if(!roomID || !username){
+      toast.error("Please Enter the Required Fields!!")
+      return
+    }
+    navigate(`/editor/${roomID}`,{
+      state:{
+        username
+      }
+    })
+  }
+  const handleInputEnter = (e)=>{
+    if(e.code === "Enter"){
+      joinRoom();
+    }
+  }
   return (
     <div className="homePageWrapper">
       <div className="formWrapper">
-        <img src="/code-sync.png" className="homePageLogo" alt="code-sync-logo" />
-        <h4 className="mainLabel">Paste Invitation Room-ID</h4>
+        <img
+          src="/code-sync.png"
+          className="homePageLogo"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste Room ID Here</h4>
         <div className="inputGroup">
-          <input type="text" className="inputBox" placeholder="Room ID" />
-          <input type="text" className="inputBox" placeholder="User Name" />
-          <button className="btn joinBtn">JOIN</button>
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="Room ID"
+            onChange={(e) => setRoomID(e.target.value)}
+            value={roomID}
+            onKeyUp={handleInputEnter}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="User Name"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            onKeyUp={handleInputEnter}
+          />
+          <button className="btn joinBtn" onClick={joinRoom}>
+            JOIN
+          </button>
           <span className="createInfo">
-            If you don't have an invite then Create &nbsp;
-            <a href="#" className="createNewBtn">
+            Create a New Collaborative Space &nbsp;
+            <a href="#" onClick={createNewRoom} className="createNewBtn">
               New Room
             </a>
           </span>
@@ -20,7 +68,8 @@ const Home = () => {
       </div>
       <footer>
         <h4>
-          Built with ❤ by <a href="https://github.com/VishalKNigam">VishalKNigam</a>
+          Crafted with 💖 by{" "}
+          <a href="https://github.com/VishalKNigam">VishalKNigam</a>
         </h4>
       </footer>
     </div>
