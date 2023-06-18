21e97bbfabc802531f6856f5d5e725cded791591
Vishal Kumar Nigam <115461865+VishalKNigam@users.noreply.github.com>
2023-06-19 00:03:25 +0530

    client.js, Editor.js, and styling part continued

--- codesync/src/pages/EditorPage.js
+++ codesync/src/pages/EditorPage.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
+
 
 const EditorPage = () => {
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Vishal K" },
+    { socketId: 2, username: "Neeraj N" }
+  ]);
   return (
-    <div>Editor Page</div>
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img
+             className="logoImage" 
+             src="/code-sync.png" 
+             alt="logo" 
+            />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => {
+              <Client key={client.socketId} username={client.username} />;
+            })}
+          </div>
+        </div>
+        <button className="btn copyBtn">Copy Room ID</button>
+        <button className="btn leaveBtn">Leave</button>
+
+
+      </div>
+      <div className="editorWrap">
+        <Editor/>
+      </div>
+    </div>
   );
 };
 
